c154a37057c8abef02cd588191b9fadb2035cd76
krepe90 <krepe90@gmail.com>
2024-05-10 23:35:42 +0900

    다모앙, 에펨코리아 크롤러 구현 추가

---
+++ crawler/damoang.py
@@ -0,0 +1,77 @@
+# 다모앙 알뜰구매 게시판 https://damoang.net/economy
+# 다모앙 알뜰구매 게시판 (RSS) https://damoang.net/bbs/rss.php?bo_table=economy
+import re
+from bs4 import BeautifulSoup
+from typing import Dict
+
+from .base_crawler import BaseCrawler, BaseArticle
+
+
+class DamoangCrawler(BaseCrawler):
+    async def parsing(self, html: str) -> Dict[int, BaseArticle]:
+        soup = BeautifulSoup(html, "html.parser")
+        data: Dict[int, BaseArticle] = {}
+
+        if (_board_name := soup.select_one(".page-title")) is None:
+            self.logger.error("Can't find board name.")
+            return data
+        if (_board_url := soup.select_one("input[name=bo_table]")) is None:
+            self.logger.error("Can't find board url.")
+            return data
+        board_name = _board_name.text.strip()
+        board_url = _board_url.attrs["value"]
+        rows = soup.select("#bo_list .list-group-item:not(.d-none)")
+
+        for row in rows:
+            # if (_id_tag := row.select_one(".wr-no")) is None:
+            #     # TODO 게시글 고유번호가 아니라 그냥 게시글 번호임;;
+            #     self.logger.warning("Cannot get article id tag")
+            #     continue
+            # if not _id_tag.text.strip().isnumeric():
+            #     continue
+            if (_title_tag := row.select_one(".flex-fill a")) is None:
+                self.logger.warning("Cannot get article title tag")
+                continue
+            if (_url := _title_tag.attrs.get("href")) is None:
+                self.logger.warning("Cannot get article url")
+                continue
+            if (_re_url := re.search(r"/([\w\d]+)/(\d+)", _url)) is None:
+                self.logger.warning("Cannot find board id and article id")
+                continue
+            if (_writer_tag := row.select_one(".wr-name a")) is None:
+                self.logger.warning("Cannot get article wrtier tag")
+                continue
+            if (_recommend_tag := row.select_one(".wr-num.order-3")) is None:
+                # hidden span tag로 "추천" 글자가 들어있음.
+                self.logger.warning("Cannot get article recommend tag")
+                continue
+            if (_view_tag := row.select_one(".wr-num.order-4")) is None:
+                # hidden span tag로 "조회" 글자가 들어있음.
+                self.logger.warning("Cannot get article view count tag")
+                continue
+            if (_status_badge_tag := row.select_one(".badge").find(text=True, recursive=False)) is None:
+                self.logger.warning("Cannot get article status badge")
+                continue
+
+            _board_id = _re_url.group(1)
+            _id = int(_re_url.group(2))
+            if _status_badge_tag.text.strip() == "종료":
+                is_end = True
+            else:
+                is_end = False
+            data[_id] = {
+                "article_id": _id,
+                "title": _title_tag.text.strip(),
+                "category": "",     # 카테고리 없음
+                "site_name": "다모앙",
+                "board_name": board_name,
+                "writer_name": _writer_tag.text.strip(),
+                "crawler_name": self.name,
+                "url": f"https://damoang.net/{_board_id}/{_id}",
+                "is_end": is_end,
+                "extra": {
+                    "recommend": "".join(_recommend_tag.find_all(text=True, recursive=False)).strip(),
+                    "view": "".join(_view_tag.find_all(text=True, recursive=False)).strip(),
+                },
+            }
+        return data
---
+++ crawler/fmkorea.py
@@ -0,0 +1,77 @@
+# 에펨코리아 핫딜 게시판 https://www.fmkorea.com/hotdeal
+from bs4 import BeautifulSoup
+from typing import Dict
+
+from .base_crawler import BaseCrawler, BaseArticle
+
+
+class FmkoreaCrawler(BaseCrawler):
+    async def parsing(self, html: str) -> Dict[int, BaseArticle]:
+        soup = BeautifulSoup(html, "html.parser")
+        data: Dict[int, BaseArticle] = {}
+        if (_board_name_tag := soup.select_one(".bd_tl h1 a")) is None:
+            self.logger.error("Can't find board name.")
+            return data
+        board_name = _board_name_tag.text.strip()
+        board_url = _board_name_tag.attrs["href"].lstrip("/")
+        rows = soup.select("#content .fm_best_widget ul li")
+
+        for row in rows:
+            if (_title_tag := row.select_one(".title a")) is None:
+                self.logger.warning("Cannot get article title tag")
+                continue
+            if (_category_tag := row.select_one(".category a")) is None:
+                self.logger.warning("Cannot get article category tag")
+                continue
+            if (_writer_tag := row.select_one(".author")) is None:
+                # "/ (이름)" 형식으로 되어있음
+                self.logger.warning("Cannot get article writer tag")
+                continue
+
+            # 추천, 댓글은 없으면 태그 자체가 없음.
+            if (_recommend_tag := row.select_one(".pc_voted_count .count")) is None:
+                _recommend = "0"
+            else:
+                _recommend = _recommend_tag.text.strip()
+            if (_comment_tag := row.select_one(".comment_count")) is None:
+                _comment = "0"
+            else:
+                _comment = _comment_tag.text.strip(" \r\n\t[]")
+            _extra = self.info_tag_parser(row.select_one(".hotdeal_info"))
+
+            if not (_id_str := _title_tag.attrs["href"].lstrip("/")).isnumeric():
+                self.logger.warning("Cannot get article id")
+                continue
+            _id = int(_id_str)
+            is_end = row.select_one(".hotdeal_var8Y") is not None
+
+            data[_id] = {
+                "article_id": _id,
+                "title": _title_tag.find(text=True, recursive=False).text.strip(),
+                "category": _category_tag.text.strip(),
+                "site_name": "에펨코리아",
+                "board_name": board_name,
+                "writer_name": _writer_tag.text.strip(" /\r\n\t"),
+                "crawler_name": self.name,
+                "url": f"https://www.fmkorea.com/{_id}",
+                "is_end": is_end,
+                "extra": {
+                    "recommend": _recommend,
+                    # "rel_url": "",
+                    **_extra,
+                    "comment": _comment,
+                },
+            }
+        return data
+    
+    def info_tag_parser(self, el: BeautifulSoup) -> Dict[str, str]:
+        # div.hotdeal_info 받아서 쇼핑몰/가격/배송 데이터 처리해 반환
+        data = {}
+        for e in el.select("span"):
+            if "쇼핑몰" in e.text:
+                data["brand"] = e.find("a").text.strip()
+            elif "가격" in e.text:
+                data["price"] = e.find("a").text.strip()
+            elif "배송" in e.text:
+                data["delivery"] = [e.find("a").text.strip()]
+        return data
