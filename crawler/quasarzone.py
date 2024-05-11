532cc2ad7a29ccfa2aec246562b342925d8eae13
krepe90 <krepe90@gmail.com>
2024-05-11 22:49:53 +0900

    퀘이사존 크롤러 추가정보 파싱과정 개선

--- crawler/quasarzone.py
+++ crawler/quasarzone.py
@@ -1,6 +1,6 @@
-# 지름/할인정보 https://quasarzone.com/bbs/qb_saleinfo
+# 핫딜 게시판 https://quasarzone.com/bbs/qb_saleinfo
 import re
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from typing import Dict
 
 from .base_crawler import BaseCrawler, BaseArticle
@@ -120,27 +120,23 @@ class QuasarzoneCrawler(BaseCrawler):
             if (_view_tag := row.select_one(".count")) is None or not _view_tag.text:
                 self.logger.warning("Cannot get view count tag")
                 continue
-            if (_category_tag := row.select_one(".category")) is None or not _category_tag.text:
-                self.logger.warning("Cannot get category tag")
+            if (_info_tag := row.select_one(".market-info-sub p:first-child")) is None:
+                self.logger.warning("Cannot get sub info tag")
                 continue
-            if (_price_tag := row.select_one(".market-info-sub .text-orange")) is None:
-                self.logger.warning("Cannot get price tag")
-                continue
-            if not (_delivery_tag := row.select(".market-info-sub > p:nth-child(1) > span:last-child")):
-                self.logger.warning("Cannot get delivery info tags")
-                continue
-            else:
-                delivery = [n.text for n in _delivery_tag if n.text]
             if (_is_end_tag := row.select_one(".label")) and _is_end_tag.text.strip() == "종료":
                 is_end = True
             else:
                 is_end = False
             _board_id = _re_url.group(1)
             _id = int(_re_url.group(2))
+            _info = self.info_tag_parser(_info_tag)
+            _category = _info.pop("category", "")
+            if not _category:
+                self.logger.warning("Cannot get category")
             data[_id] = {
                 "article_id": _id,
                 "title": _title_tag.text.strip(),
-                "category": _category_tag.text.strip(),
+                "category": _category,
                 "site_name": "퀘이사존",
                 "board_name": board_name,
                 "writer_name": _nick_tag.text.strip(),
@@ -150,8 +146,38 @@ class QuasarzoneCrawler(BaseCrawler):
                 "extra": {
                     "recommend": _recommend_tag.text.strip(),
                     "view": _view_tag.text.strip(),
-                    "price": _price_tag.text.strip(),
-                    "delivery": delivery
+                    **_info     # price, delivery, direct_delivery
                 }
             }
         return data
+    
+    def info_tag_parser(self, el: Tag) -> Dict[str, str]:
+        """div.market-info-sub p:first-child 받아서 쇼핑몰/가격/배송 데이터 처리해 반환
+
+        Args:
+            el (BeautifulSoup): div.market-info-sub p:first-child
+
+        Returns:
+            Dict[str, str]: 쇼핑몰/가격/배송 데이터
+        """
+        data = {}
+        for e in el.find_all("span", recursive=False):
+            if not isinstance(e, Tag):
+                continue
+            if "category" in e.attrs.get("class", []):
+                # 카테고리 태그 - PC/하드웨어
+                data["category"] = e.text.strip()
+            elif e.find(text=True, recursive=False) == "가격":
+                # 가격 태그 - ￦ 121,687 (KRW)
+                data["price"] = e.find("span").text.strip()
+            elif "brand" in e.attrs.get("class", []):
+                # TODO 쇼핑몰 아이콘으로 이름 가져오기
+                continue
+            elif e.find(text=True, recursive=False).strip() == "직배":
+                # 해외 배송 직배 가능 여부 태그 - 가능 / 불가능
+                data["direct_delivery"] = True if "가능" in e.text else False
+            elif "배송비" in e.text.strip():
+                # 배송비 태그 - 배송비 {텍스트}
+                data["delivery"] = [e.text.replace("배송비", "").strip()]
+        return data
+    
