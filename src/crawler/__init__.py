69d3340771c44482009048efd84a32f0326d4900
krepe90 <krepe90@gmail.com>
2024-10-28 23:21:53 +0900

    test: 크롤러 테스트 코드 추가

--- src/crawler/__init__.py
+++ src/crawler/__init__.py
@@ -3,7 +3,7 @@ from .clien import ClienCrawler
 from .dummy import DummyCrawler
 from .damoang import DamoangCrawler
 from .fmkorea import FmkoreaCrawler
-from .ppomppu import PpomppuCrawler
+from .ppomppu import PpomppuCrawler, PpomppuRSSCrawler
 from .ruliweb import RuliwebCrawler
 from .arcalive import ArcaLiveCrawler
 from .coolenjoy import CoolenjoyCrawler, CoolenjoyRSSCrawler
--- tests/test_crawlers.py
+++ tests/test_crawlers.py
@@ -23,6 +23,7 @@ async def session():
 
 @pytest.mark.asyncio
 async def test_crawler_arca(session):
+    """아카라이브 핫딜 채널 크롤링 테스트 수행"""
     crawler_instance = crawler.ArcaLiveCrawler("arcalive_hotdeal", ["https://arca.live/b/hotdeal"], session=session)
     data: crawler.ArticleCollection = await crawler_instance.get()
     assert len(data) > 0
@@ -30,6 +31,55 @@ async def test_crawler_arca(session):
 
 @pytest.mark.asyncio
 async def test_cralwer_ppomppu(session):
+    """뽐뿌 뽐뿌게시판 크롤링 테스트 수행"""
     crawler_instance = crawler.PpomppuCrawler("ppomppu_crawler", ["https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu"], session=session)
+    data: crawler.ArticleCollection = await crawler_instance.get()
+    assert len(data) > 0
+
+
+@pytest.mark.asyncio
+async def test_crawler_ppomppu_rss(session):
+    """뽐뿌 뽐뿌게시판 RSS 크롤링 테스트 수행"""
+    crawler_instance = crawler.PpomppuRSSCrawler("ppomppu_rss_crawler", ["https://www.ppomppu.co.kr/rss.php?id=ppomppu"], session=session)
+    data: crawler.ArticleCollection = await crawler_instance.get()
+    assert len(data) > 0
+
+
+@pytest.mark.asyncio
+async def test_crawler_ruliweb(session):
+    """루리웹 예구핫딜 게시판 크롤링 테스트 수행"""
+    crawler_instance = crawler.RuliwebCrawler("ruliweb_crawler", ["https://bbs.ruliweb.com/community/board/300143"], session=session)
+    data: crawler.ArticleCollection = await crawler_instance.get()
+    assert len(data) > 0
+
+
+@pytest.mark.asyncio
+async def test_crawler_clien(session):
+    """클리앙 알뜰구매 게시판 크롤링 테스트 수행"""
+    crawler_instance = crawler.ClienCrawler("clien_crawler", ["https://www.clien.net/service/board/jirum"], session=session)
+    data: crawler.ArticleCollection = await crawler_instance.get()
+    assert len(data) > 0
+
+
+@pytest.mark.asyncio
+async def test_crawler_damoang(session):
+    """다모앙 알뜰구매 게시판 크롤링 테스트 수행"""
+    crawler_instance = crawler.DamoangCrawler("damoang_crawler", ["https://damoang.net/economy"], session=session)
+    data: crawler.ArticleCollection = await crawler_instance.get()
+    assert len(data) > 0
+
+
+@pytest.mark.asyncio
+async def test_crawler_quasarzone(session):
+    """퀘이사존 핫딜 게시판 크롤링 테스트 수행"""
+    crawler_instance = crawler.QuasarzoneCrawler("quasarzone_crawler", ["https://quasarzone.com/bbs/qb_saleinfo"], session=session)
+    data: crawler.ArticleCollection = await crawler_instance.get()
+    assert len(data) > 0
+
+
+@pytest.mark.asyncio
+async def test_crawler_fmkorea(session):
+    """에펨코리아 핫딜 게시판 크롤링 테스트 수행"""
+    crawler_instance = crawler.FmkoreaCrawler("fmkorea_crawler", ["https://www.fmkorea.com/hotdeal"], session=session)
     data: crawler.ArticleCollection = await crawler_instance.get()
     assert len(data) > 0
