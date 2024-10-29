beff7982e44a8f86982c21d27708893b7d2e294f
krepe90 <krepe90@gmail.com>
2024-10-29 23:21:49 +0900

    style: tools.crawler 스타일 적용

--- tools/crawler.py
+++ tools/crawler.py
@@ -1,12 +1,13 @@
 """커맨드라인에서 사용할 수 있는 CLI 크롤러 도구, 크롤링 테스트용으로 사용 가능.
 """
+
 import asyncio
 import typer
 
 from src import crawler
 
 
-async def main(module_name: str):
+async def main(module_name: str, detail: bool = False):
     """크롤러 도구의 메인 함수.
 
     Args:
@@ -14,17 +15,32 @@ async def main(module_name: str):
         url (str | None): 크롤링할 URL.
     """
     if module_name == "arca":
-        crawler_instance = crawler.ArcaLiveCrawler("arcalive_hotdeal", ["https://proxy.krepe90.workers.dev/proxy?proxyUrl=https://arca.live/b/hotdeal"])
+        crawler_instance = crawler.ArcaLiveCrawler(
+            "arcalive_hotdeal",
+            ["https://arca.live/b/hotdeal"],
+        )
     elif module_name == "ppomppu":
-        crawler_instance = crawler.PpomppuCrawler("ppomppu_crawler", ["https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu"])
+        crawler_instance = crawler.PpomppuCrawler(
+            "ppomppu_crawler",
+            ["https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu"],
+        )
     elif module_name == "ppomppu_rss":
-        crawler_instance = crawler.PpomppuRSSCrawler("ppomppu_rss_crawler", ["https://www.ppomppu.co.kr/rss.php?id=ppomppu"])
+        crawler_instance = crawler.PpomppuRSSCrawler(
+            "ppomppu_rss_crawler", ["https://www.ppomppu.co.kr/rss.php?id=ppomppu"]
+        )
     elif module_name == "ruliweb":
-        crawler_instance = crawler.RuliwebCrawler("ruliweb_crawler", ["https://bbs.ruliweb.com/market/board/1020?view=thumbnail&page=1"])
+        crawler_instance = crawler.RuliwebCrawler(
+            "ruliweb_crawler",
+            ["https://bbs.ruliweb.com/market/board/1020?view=thumbnail&page=1"],
+        )
     elif module_name == "clien":
-        crawler_instance = crawler.ClienCrawler("clien_crawler", ["https://www.clien.net/service/board/jirum"])
+        crawler_instance = crawler.ClienCrawler(
+            "clien_crawler", ["https://www.clien.net/service/board/jirum"]
+        )
     elif module_name == "damoang":
-        crawler_instance = crawler.DamoangCrawler("damoang_crawler", ["https://damoang.net/economy"])
+        crawler_instance = crawler.DamoangCrawler(
+            "damoang_crawler", ["https://damoang.net/economy"]
+        )
     else:
         raise ValueError(f"Unknown module name: {module_name}")
 
