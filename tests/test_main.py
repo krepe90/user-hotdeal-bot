fef40421ba3fd36ffe8f3c641ee0ac494251c8f6
krepe90 <krepe90@gmail.com>
2025-05-22 21:53:48 +0900

    test: pyproject.toml 버전 테스트 추가

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+import tomllib
+
+
+def test_version():
+    from src.main import __version__
+    with open("pyproject.toml", "rb") as f:
+        pyproject = tomllib.load(f)
+    project_version = pyproject["project"]["version"]
+    assert project_version == __version__
